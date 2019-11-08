c7bdb35d6778244994c0405b6a9d226005e637ba
chelblasz <chelseablaszczyk@gmail.com>
2019-11-08 09:55:37 -0700

    updates

--- cit230/lesson8/scripts/menu.js
+++ cit230/lesson8/scripts/menu.js
@@ -1,3 +1,10 @@
 function toggleMenu() {
 	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
+
+
+//Rating line
+
+function adjustRating(rating) {
+    document.getElementById("ratingvalue").innerHTML = rating;
+}
---
+++ cit230/lesson9/scripts/getdate.js
@@ -0,0 +1,27 @@
+var currentDateString;
+
+var currentDate = new Date();
+//get day of week
+
+
+var weekDayDate = currentDate.getDay();
+//get name of the day
+var daysOfWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+var weekDay = daysOfWeek[weekDayDate];
+
+currentDateString = weekDay + ", ";
+
+currentDateString += currentDate.getDate();
+//get name of the month
+var months = ["January","February","March","April","May","June","july","August","September","October","November","December"];
+
+var monthNumber = currentDate.getMonth();
+
+var month = months[monthNumber];
+
+currentDateString += " " + month;
+
+currentDateString += " " + currentDate.getFullYear();
+
+document.getElementById("currentDate").innerHTML = currentDateString;
