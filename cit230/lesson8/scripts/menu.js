ecf3ada34d3ed5721a13cdc6293600309ce601ac
chelblasz <chelseablaszczyk@gmail.com>
2019-11-08 13:07:33 -0700

    updates

--- cit230/lesson8/scripts/menu.js
+++ cit230/lesson8/scripts/menu.js
@@ -8,3 +8,10 @@ function toggleMenu() {
 function adjustRating(rating) {
     document.getElementById("ratingvalue").innerHTML = rating;
 }
+var slider = document.getElementById("myRange");
+var output = document.getElementById("ratingValue");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
