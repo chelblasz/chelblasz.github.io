bd0f8ed9d9098ae1718ed5eb33fa9ddf7a175b6f
chelblasz <chelseablaszczyk@gmail.com>
2019-11-20 12:43:19 -0700

    lesson 10

--- cit230/lesson10/scripts/maindate.js
+++ cit230/lesson10/scripts/maindate.js
@@ -6,7 +6,7 @@ var currentDate = new Date();
 
 var weekDayDate = currentDate.getDay();
 //get name of the day
-var daysOfWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 var weekDay = daysOfWeek[weekDayDate];
 
@@ -14,7 +14,7 @@ currentDateString = weekDay + ", ";
 
 currentDateString += currentDate.getDate();
 //get name of the month
-var months = ["January","February","March","April","May","June","july","August","September","October","November","December"];
+var months = ["January", "February", "March", "April", "May", "June", "july", "August", "September", "October", "November", "December"];
 
 var monthNumber = currentDate.getMonth();
 
@@ -30,19 +30,75 @@ document.getElementById("currentDate").innerHTML = currentDateString;
 // create 5 day forecast
 let currentDay = weekDayDate;
 
-for (let i = 1; i < 6; i++){
+for (let i = 1; i < 6; i++) {
     //add one day to current day 
     currentDay++;
 
 
-//if current day is greater than 6 (Saturday), reset to 0
-if (currentDay > 6) {
-    currentDay = 0;
+    //if current day is greater than 6 (Saturday), reset to 0
+    if (currentDay > 6) {
+        currentDay = 0;
+    }
+
+    //assign value to placeholder in HTML
+    const element = document.getElementById(`day${i}`);
+
+    // element.innerHTML = daysOfWeek[currentDay];
+
 }
 
-//assign value to placeholder in HTML
-const element = document.getElementById(`day${i}`);
+// FETCH THE DATA
+//  function getWeather()
+const apiForecastURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=a4de96eb4c9cdd8c0998596fbb5ae04d&units=imperial';
 
-// element.innerHTML = daysOfWeek[currentDay];
+fetch(apiForecastURL)
+    .then(
+        (response) => response.json()
+    )
+    .then(
+        (forecasts) => {
 
-}
+            //debugging console.log
+            console.log(forecasts);
+            //_____________________________________________________________ 
+
+            let tomorrowDate = new Date();
+            tomorrowDate.setDate(tomorrowDate.getDate() + 1);
+
+            let dateString =
+                //find full year
+                tomorrowDate.getFullYear() + "-" +
+
+                //get month
+                (tomorrowDate.getMonth() + 1) + "-" +
+                //get day
+                tomorrowDate.getDate();
+
+                //get hour
+                let hourString = "18:00:00";
+
+                //debugging
+                console.log(dateString);
+
+                let counter = 1;
+
+                //loop through results
+                forecasts.list.forEach(
+                (forecast) => {
+
+                    if (forecast.dt_txt.includes(dateString) &&
+                    forecast.dt_txt.includes(hourString)) { 
+                    
+                     //DEBUGGING
+                    console.log(forecast.main.temp);
+                            
+
+                    const element = document.getElementById(`temp${counter}`);
+                    element.innerHTML = forecast.main.temp + '&deg';
+
+                    counter += 1;
+                }
+            }
+            )
+
+        });
--- cit230/lesson10/scripts/windchill.js
+++ cit230/lesson10/scripts/windchill.js
@@ -1,17 +1,30 @@
-let currentTemp = 41;
-let windSpeed = 8;
-let windChill = 0;
+//  function getWeather()
+const apiWeatherURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=a4de96eb4c9cdd8c0998596fbb5ae04d&units=imperial';
 
-if (currentTemp <= 50 && windSpeed > 3){
-    windChill = 35.74 + 0.6215 * currentTemp - 35.75 * Math.pow(windSpeed, 0.16) + .4275 * currentTemp * Math.pow(windSpeed, .16);
-}
+fetch(apiWeatherURL)
+    .then(
+        (response) => response.json()
+    )
+    .then(
+        (currentWeather) => {
+            //debugging console.log
+            // console.log(currentWeather.main.temp);
 
-document.getElementById("currentTemp").innerHTML = currentTemp + "&deg;";
-document.getElementById("windSpeed").innerHTML = windSpeed + " mph";
+            let currentTemp = currentWeather.main.temp;
+            let windSpeed = currentWeather.wind.speed;
+            let windChill;
 
-if(windChill === 0){
-    document.getElementById("windChill").innerHTML = "N/A";
-}
-else{
-    document.getElementById("windChill").innerHTML = Math.round(windChill) + "&deg;";
-}
+            if (currentTemp <= 50 && windSpeed > 3) {
+                windChill = 35.74 + 0.6215 * currentTemp - 35.75 * Math.pow(windSpeed, 0.16) + .4275 * currentTemp * Math.pow(windSpeed, .16);
+            }
+
+            document.getElementById("currentTemp").innerHTML = currentTemp + "&deg;";
+            document.getElementById("windSpeed").innerHTML = windSpeed + " mph";
+
+            if (windChill === 0) {
+                document.getElementById("windChill").innerHTML = "N/A";
+            } else {
+                document.getElementById("windChill").innerHTML = Math.round(windChill) + "&deg;";
+            }
+
+        });
