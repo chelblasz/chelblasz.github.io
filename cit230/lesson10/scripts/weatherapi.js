b3c251440204f7990d770c06f89fe115341240c2
chelblasz <chelseablaszczyk@gmail.com>
2019-11-22 10:25:30 -0700

    api

--- cit230/lesson10/scripts/weatherapi.js
+++ cit230/lesson10/scripts/weatherapi.js
@@ -1,14 +1,14 @@
 const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=a4de96eb4c9cdd8c0998596fbb5ae04d&units=imperial';
 
 fetch(apiURL)
-  .then((response) => response.json()) 
+  .then((response) => response.json())
   .then((currentWeather) => {
     //debugging console.log
     console.log(currentWeather.main.temp);
 
     let currentTemp = currentWeather.main.temp;
-let currentImageIcon = currentWeather.weather[0].icon;
-let currentImage = 'https://openweathermap.org/img/w/' + currentImageIcon + '.png'; 
+    let currentImageIcon = currentWeather.weather[0].icon;
+    let currentImage = 'https://openweathermap.org/img/w/' + currentImageIcon + '.png';
 
 
     document.querySelector('#current-temp').textContent = currentTemp;
@@ -18,5 +18,4 @@ let currentImage = 'https://openweathermap.org/img/w/' + currentImageIcon + '.pn
     document.querySelector('#weather-icon').setAttribute('src', currentImage);
 
     document.querySelector('#weather-icon').setAttribute('alt', currentWeather.weather[0].main);
-  }
-  );
+  });
